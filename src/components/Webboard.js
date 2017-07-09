6e807a4248ba1fe87ab05cdedc72915f057f1931
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-07-09 22:42:46 +0700

    Remove solution

--- src/components/Webboard.js
+++ src/components/Webboard.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import TopicList from './TopicList'
-import TopicDetail from './TopicDetail'
-import PageNotFound from './PageNotFound'
 import * as api from '../api'
 
 const Webboard = () =>
   <div className="webboard">
     <div className="title">CS Webboard</div>
-    <Switch>
-      <Route exact path="/topics" component={TopicList} />
-      <Route path="/topics/:id" component={TopicDetail} />
-      <Redirect from="/" to="/topics" />
-      <Route component={PageNotFound} />
-    </Switch>
+    {/* Add route handler here */}
   </div>
 
 export default Webboard
